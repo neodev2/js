1fdbea36355be145439aaf4c72ed9de35efe3091
neodev2 <neodev2@gmail.com>
2017-04-26 12:58:26 +0200

    Update draggable.js

--- draggable.js
+++ draggable.js
@@ -1,33 +1,33 @@
 function draggableMode(selector, extraCss){
-	let isMouseDown = false;
-	let elems = document.querySelectorAll(selector);
-	let elem, l, t;
+    let isMouseDown = false;
+    let elems = document.querySelectorAll(selector);
+    let elem, l, t;
 
-	for(let i=0; i<elems.length; i++){
+    for(let i=0; i<elems.length; i++){
 
-		elems[i].ondragstart = function(e){
-			e.preventDefault();
-		}
+        elems[i].ondragstart = function(e){
+            e.preventDefault();
+        }
 
-		elems[i].onmousedown = function(e){
-			isMouseDown = true;
-			elem = this;
-			l = e.clientX - this.getBoundingClientRect().left;
-			t = e.clientY - this.getBoundingClientRect().top;
-		}
-	}
+        elems[i].onmousedown = function(e){
+            isMouseDown = true;
+            elem = this;
+            l = e.clientX - this.getBoundingClientRect().left;
+            t = e.clientY - this.getBoundingClientRect().top;
+        }
+    }
 
-	document.onmouseup = function(){
-		isMouseDown = false;
-	}
+    document.onmouseup = function(){
+        isMouseDown = false;
+    }
 
-	document.onmousemove = function(e){
-		if(isMouseDown){
-			elem.style.left = e.clientX - l +'px';
-			elem.style.top = e.clientY - t +'px';
-			for(let i in extraCss){
-				elem.style[i] = extraCss[i];
-			}
-		}
-	}
+    document.onmousemove = function(e){
+        if(isMouseDown){
+            elem.style.left = e.clientX - l +'px';
+            elem.style.top = e.clientY - t +'px';
+            for(let i in extraCss){
+                elem.style[i] = extraCss[i];
+            }
+        }
+    }
 }
